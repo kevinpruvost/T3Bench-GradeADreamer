937cdbfbbba1f17006b6cb670b2dc572ce0797ff
kevinpruvost <kevin.pruvost@epitech.eu>
2024-08-14 20:03:48 +0800

    outputs updated

--- run_caption.py
+++ run_caption.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import glob
 from copy import deepcopy
+import regex
 
 # import openai
 #import backoff
@@ -34,10 +35,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import torch
 
 # Load model and tokenizer
-model_name = "NousResearch/Meta-Llama-3-8B-Instruct"  # or your chosen model
+model_name = "Qwen/CodeQwen1.5-7B-Chat"  # or your chosen model
 
-prompt_to_llm_ori = 'Given a set of descriptions about the same 3D object, distill these descriptions into one concise caption. The descriptions are as follows:\n\n'
-prompt_to_llm_ori += '\nAvoid describing background, surface, and posture and Answer directly with the distillation alone, no useless sentences.'
+prompt_to_llm_ori = 'Given a set of descriptions about the same 3D object, distill these descriptions into one short concise caption.'
+prompt_to_llm_ori += 'Do not talk about background, surface, no useless sentences. Give only about 6 words and answer directly, no paraphrasing.'
+prompt_to_llm_ori += "Answer in the format: 'A horse on a grass field'"
 
 def predict(model_llm, tokenizer, pipe_llm, prompt, temperature):
     chat = [
@@ -46,8 +48,7 @@ def predict(model_llm, tokenizer, pipe_llm, prompt, temperature):
     ]
 
     with torch.no_grad():
-        outputs = pipe_llm(chat, max_new_tokens=512, do_sample=False, temperature=temperature)
-    print(outputs[0]['generated_text'][2]['content'])
+        outputs = pipe_llm(chat, max_new_tokens=16, do_sample=False, temperature=temperature)
     return outputs[0]['generated_text'][2]['content']
 
     # inputs = tokenizer(prompt, return_tensors="pt")
@@ -68,74 +69,103 @@ def predict(model_llm, tokenizer, pipe_llm, prompt, temperature):
 
 # The rest of your code remains the same
 
-
 def run(args, temp_path):
+    # set CUDA_VISIBLE_DEVICES
+    # os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
     device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
     torch.cuda.set_device(device)
+    print(f"Using device: {device}")
+
+    # if prompt inputs are already stored, load them
+    prompt_inputs = {}
+    if os.path.exists(f'outputs_caption/{args.method}_views.txt'):
+        with open(f'outputs_caption/{args.method}_views.txt') as f:
+            lines = f.readlines()
+            prompt_input = ""
+            for line in lines:
+                # if view#: is found, # is any number, use regex
+                if not regex.match(r'view\d+:', line):
+                    if prompt_input != "":
+                        prompt_inputs[prompt] = prompt_input
+                    prompt, _ = line.split(':', 1)
+                    prompt_input = ""
+                else:
+                    prompt_input += line
+            if prompt_input != "":
+                prompt_inputs[prompt] = prompt_input
+    else:
+        model, vis_processors, _ = load_model_and_preprocess(name='blip2_t5', model_type='pretrain_flant5xxl', is_eval=True, device="cuda")
+
+        Radius = 2.2
+        icosphere = trimesh.creation.icosphere(subdivisions=0)
+        icosphere.vertices *= Radius
+
+        with open(f'prompts_to_evaluate.txt') as f:
+            lines = f.readlines()
+
+        os.makedirs(f'outputs_caption', exist_ok=True)
+
+        # remove output file first
+        try:
+            os.remove(f'outputs_caption/{args.method}.txt')
+        except FileNotFoundError:
+            pass
+
+        for prompt in lines:
+            prompt = prompt.strip()
+            prompt_formatted = prompt.replace(' ', '_')
+            try:
+                obj_path = glob.glob(f'outputs_mesh_t3/{args.method}/{prompt_formatted}/mesh.obj')[-1].replace("\'", "\\\'")
+            except IndexError:
+                raise ValueError(f"Mesh not found for prompt: {prompt}")
+            
+            os.system(f'python render/meshrender_cap.py --path {obj_path} --name {temp_path}')
+
+            texts = []
+
+            for idx, img_path in enumerate(os.listdir(temp_path)):
+                color = Image.open(os.path.join(temp_path, img_path)).convert("RGB")
+                image = vis_processors["eval"](color).unsqueeze(0).to("cuda")
+                x = model.generate({"image": image}, use_nucleus_sampling=True, num_captions=1)
+                texts += x
+
+            prompt_input = ""
+            for idx, txt in enumerate(texts):
+                prompt_input += f'view{idx+1}: '
+                prompt_input += txt
+                prompt_input += '\n'
+            prompt_inputs[prompt] = prompt_input
+
+        # store prompt inputs
+        with open(f'outputs_caption/{args.method}_views.txt', 'w+') as f:
+            for prompt, prompt_input in prompt_inputs.items():
+                f.write(prompt + ':\n' + prompt_input)
+
+        # free blip
+        del model
+        del vis_processors
+
+    # Load model and tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model_llm = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
     pipe_llm = pipeline("text-generation", model=model_llm, tokenizer=tokenizer)
-    model, vis_processors, _ = load_model_and_preprocess(name='blip2_t5', model_type='pretrain_flant5xxl', is_eval=True, device=device)
 
-    Radius = 2.2
-    icosphere = trimesh.creation.icosphere(subdivisions=0)
-    icosphere.vertices *= Radius
+    # predict
+    for prompt, prompt_input in prompt_inputs.items():
+        res = predict(model_llm, tokenizer, pipe_llm, prompt_input, 0)
+        print(f"Prompt: [{prompt}], Prompt Input: [{prompt_input}] Result: [{res}]")
 
-    with open(f'data/prompt_{args.group}.txt') as f:
-        lines = f.readlines()
-
-    os.makedirs(f'outputs_caption', exist_ok=True)
-    
-    # remove output file first
-    try:
-        os.remove(f'outputs_caption/{args.method}_{args.group}.txt')
-    except FileNotFoundError:
-        pass
-
-    for prompt in lines:
-        prompt = prompt.strip()
-        try:
-            obj_path = glob.glob(f'outputs_mesh_t3/{args.method}/{args.group}/mesh.obj')[-1].replace("\'", "\\\'")
-        except IndexError:
-            obj_path = 'outputs_mesh_t3/FALSE_PATH'
-        
-        os.system(f'python render/meshrender_cap.py --path {obj_path} --name {temp_path}')
-
-        texts = []
-
-        for idx, img_path in enumerate(os.listdir(temp_path)):
-            color = Image.open(os.path.join(temp_path, img_path)).convert("RGB")
-            image = vis_processors["eval"](color).unsqueeze(0).to(device)
-            x = model.generate({"image": image}, use_nucleus_sampling=True, num_captions=1)
-            texts += x
-    
-        prompt_input = ""
-        for idx, txt in enumerate(texts):
-            prompt_input += f'view{idx+1}: '
-            prompt_input += txt
-            prompt_input += '\n'
-        res = predict(model_llm, tokenizer, pipe_llm, prompt_input, 0.6)
-
-        with open(f'outputs_caption/{args.method}_{args.group}.txt', 'a+') as f:
+        with open(f'outputs_caption/{args.method}.txt', 'a+') as f:
             f.write(prompt + ':' + res + '\n')
         
-        
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--group', type=str, default='single', choices=['astro'])
-    parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--method', type=str, choices=['magic3d', 'fantasia3d', 'prolificdreamer', 'gradeadreamer'])
+    parser.add_argument('--gpu', required=True, type=int, default=0)
+    parser.add_argument('--method', required=True, type=str, choices=['magic3d', 'fantasia3d', 'dreamfusion', 'prolificdreamer', 'gradeadreamer'])
     args = parser.parse_args()
 
-    i = 0
-    while True:
-        try: 
-            temp_path = f'temp/temp_{i}'
-            os.makedirs(temp_path)
-            break
-        except:
-            i += 1
+    temp_path = f'temp/temp_{args.method}'
+    os.makedirs(temp_path, exist_ok=True)
     
     run(args, temp_path)
     os.system(f'rm -r {temp_path}')
--- run_eval_alignment.py
+++ run_eval_alignment.py
@@ -9,25 +9,26 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import torch
 
 # Load model and tokenizer
-model_name = "NousResearch/Meta-Llama-3-8B-Instruct"  # or your chosen model
+model_name = "Qwen/CodeQwen1.5-7B-Chat"  # or your chosen model
 
-def predict(model_llm, tokenizer, prompt, temperature):
-    inputs = tokenizer(prompt, return_tensors="pt")
+prompt_to_llm_ori = '''You are an assessment expert responsible for prompt-prediction pairs. Your task is to score the prediction according to the following requirements:
+Evaluate the recall, or how well the prediction covers the information in the prompt.
+
+1. If the prediction contains information that does not appear in the prompt, it should not be considered as bad.
+2. If the prediction contains correct information about color or features in the prompt, you should also consider raising your score.
+3. Assign a score between 1 and 5, with 5 being the highest. Do not provide a complete answer; give the score in the format: 5
+'''
+
+def predict(model_llm, pipe_llm, tokenizer, prompt, temperature):
+    chat = [
+        {"role": "system", "content": prompt_to_llm_ori},
+        {"role": "user", "content": prompt}
+    ]
 
     with torch.no_grad():
-        outputs = model_llm.generate(
-            **inputs,
-            max_new_tokens=15,  # adjust as needed
-            temperature=temperature,
-            do_sample=True,
-            top_p=0.6,
-        )
-    
-    full_response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # Extract only the model's response
-    answer = full_response[len(prompt):].strip()
-    print(f"full_response: {answer}")
-    return answer
+        outputs = pipe_llm(chat, max_new_tokens=512, do_sample=False, temperature=temperature)
+    print(outputs[0]['generated_text'][2]['content'])
+    return outputs[0]['generated_text'][2]['content']
 
 def process_text(text):
     if text[-1] == '.':
@@ -45,22 +46,14 @@ def run(args, temp_path):
     torch.cuda.set_device(device)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model_llm = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
-
+    pipe_llm = pipeline("text-generation", model=model_llm, tokenizer=tokenizer)
     mean_score = 0
     output = ''
 
-    prompt_to_llm_ori = '''You are an assessment expert responsible for prompt-prediction pairs. Your task is to score the prediction according to the following requirements:
-
-    1. Evaluate the recall, or how well the prediction covers the information in the prompt. If the prediction contains information that does not appear in the prompt, it should not be considered as bad.
-    2. If the prediction contains correct information about color or features in the prompt, you should also consider raising your score.
-    3. Assign a score between 1 and 5, with 5 being the highest. Do not provide a complete answer; give the score in the format: 3
-
-    '''
-
-    with open(f'outputs_caption/{args.method}_{args.group}.txt') as f:
+    with open(f'outputs_caption/{args.method}.txt') as f:
         lines = f.readlines()
 
-    os.makedirs(f'result/alignment', exist_ok=True)
+    os.makedirs(f'outputs_alignment', exist_ok=True)
     
     for line in lines:
         line = line.strip()
@@ -68,12 +61,10 @@ def run(args, temp_path):
         print(prompt, text)
         text = process_text(text)
 
-        prompt_to_llm = prompt_to_llm_ori
-        prompt_to_llm += 'Prompt: ' + prompt + '\n'
+        prompt_to_llm = 'Prompt: ' + prompt + '\n'
         prompt_to_llm += 'Prediction: ' + text
         prompt_to_llm += '\nOnly give a number and nothing else. For example, if you think the prediction is a 3, type 3.'
-        print(prompt_to_llm)
-        res = predict(model_llm, tokenizer, prompt_to_llm, 0.6)
+        res = predict(model_llm, pipe_llm, tokenizer, prompt_to_llm, 0)
     
         print(res)
         mean_score += np.round(float(res)) / len(lines)
@@ -81,14 +72,13 @@ def run(args, temp_path):
 
     print("Alignment Score:", mean_score)
 
-    with open(f'result/alignment/{args.method}_{args.group}.txt', 'a+') as f:
+    with open(f'outputs_alignment/{args.method}.txt', 'a+') as f:
         f.write(output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--group', type=str, default='single', choices=['astro'])
     parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--method', type=str, choices=['magic3d', 'fantasia3d', 'prolificdreamer', 'gradeadreamer'])
+    parser.add_argument('--method', required=True, type=str, choices=['magic3d', 'fantasia3d', 'dreamfusion', 'prolificdreamer', 'gradeadreamer'])
     args = parser.parse_args()
 
     i = 0
